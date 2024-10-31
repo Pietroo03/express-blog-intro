c6b0b0de1bfcfda60b2d7432a0c33e0d4654b82d
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-31 14:53:00 +0100

    create file posts.js
    adding array of objects

---
+++ controllers/posts.js
@@ -0,0 +1,32 @@
+const posts = [
+    {
+        titolo: 'Giornata al mare',
+        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        immagine: 'https://picsum.photos/200/300',
+        tags: ['sun', 'sea', 'summer', 'holiday']
+    },
+    {
+        titolo: 'Giornata al parco',
+        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        immagine: 'https://picsum.photos/200/300',
+        tags: ['sunny', 'freedom', 'hobby', 'weekend']
+    },
+    {
+        titolo: 'Pizza fatta in casa',
+        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        immagine: 'https://picsum.photos/200/300',
+        tags: ['pizza', 'free-time', 'cooking', 'Italia', 'MammaMia!']
+    },
+    {
+        titolo: 'Imparando JavaScript',
+        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        immagine: 'https://picsum.photos/200/300',
+        tags: ['happy', 'PC', 'hardwork', 'super']
+    },
+    {
+        titolo: 'Andando dallo psicologo',
+        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        immagine: 'https://picsum.photos/200/300',
+        tags: ['JS', 'esausto', 'grazieFabio', 'happy']
+    },
+]
