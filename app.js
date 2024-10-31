a39f70a0c7c492a05db866c260a1f81c912c6e03
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-31 15:01:06 +0100

    add module.export to posts.js
    add require to app.js
    
    add route "posts" to app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const posts = require('./controllers/posts')
+const { log } = require('console')
 const app = express()
 const host = 'http://127.0.0.1'
 const port = 3000
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on ${host}:${port}`)
+})
 
 app.get('/', (req, res) => {
     res.send('<h1>Benvenuto nel mio Blog!<h1>')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on ${host}:${port}`)
+app.get('/posts', (req, res) => {
+    res.send(posts)
 })
--- controllers/posts.js
+++ controllers/posts.js
@@ -30,3 +30,5 @@ const posts = [
         tags: ['JS', 'esausto', 'grazieFabio', 'happy']
     },
 ]
+
+module.exports = posts
