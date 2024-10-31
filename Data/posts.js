984445e4e3a5aaf89e38a1da45881c7956e68a50
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-31 15:39:21 +0100

    add "public" folder
    modified posts.js

--- Data/posts.js
+++ Data/posts.js
@@ -2,31 +2,31 @@ const posts = [
     {
         titolo: 'Giornata al mare',
         contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-        immagine: 'https://picsum.photos/200/300',
+        immagine: './img/mare.jpg',
         tags: ['sun', 'sea', 'summer', 'holiday']
     },
     {
         titolo: 'Giornata al parco',
         contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-        immagine: 'https://picsum.photos/200/300',
+        immagine: './img/parco.jpg',
         tags: ['sunny', 'freedom', 'hobby', 'weekend']
     },
     {
         titolo: 'Pizza fatta in casa',
         contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-        immagine: 'https://picsum.photos/200/300',
+        immagine: './img/pizza.jpg',
         tags: ['pizza', 'free-time', 'cooking', 'Italia', 'MammaMia!']
     },
     {
         titolo: 'Imparando JavaScript',
         contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-        immagine: 'https://picsum.photos/200/300',
+        immagine: './img/javascript.jpg',
         tags: ['happy', 'PC', 'hardwork', 'super']
     },
     {
         titolo: 'Andando dallo psicologo',
         contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-        immagine: 'https://picsum.photos/200/300',
+        immagine: './img/psicologo.jpg',
         tags: ['JS', 'esausto', 'grazieFabio', 'happy']
     },
 ]
