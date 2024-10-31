ef27b74c1f2b29bee7c2ade939e489aabfbdeb10
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-31 14:36:18 +0100

    create server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('ciao');
+const express = require('express')
+const app = express()
+const host = 'http://127.0.0.1'
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('<h1>Benvenuto nel mio Blog!<h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on ${host}:${port}`)
+})
