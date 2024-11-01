42765409033eca077a4b429fd803ef21e3072eee
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-01 16:41:01 +0100

    add function markup to main.js
    add css style

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,43 @@
-
+const rowEl = document.querySelector('.row')
 
 axios.get("http://127.0.0.1:3000/posts")
     .then(response =>  {
         const posts = response.data.data
         console.log(posts);
+        let markup = create_markup(posts)
+        rowEl.innerHTML = markup
+
+
     })
 
 
+
+
+function create_markup(posts) {
+    let postElement = ''
+    posts.forEach(post => {
+        const {titolo, contenuto, immagine, tags} = post
+        console.log(post);
+        const markup = `
+        <div class="post col-12 col-md-5 m-3">
+            <div class="immagine">
+                <img src="${immagine}" alt="">
+            </div>
+            <h3 class="title pt-3">
+                 ${titolo}
+            </h3>
+            <div class="description">
+                ${contenuto}
+            </div>
+            <div class="tags py-2">
+                ${tags}
+            </div>
+         </div>
+        `
+
+        postElement += markup
+    });
+
+    console.log(postElement);
+    return postElement
+}
