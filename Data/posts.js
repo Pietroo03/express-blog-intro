82e6412481df6a36abba3ad685b24f8d29c724c8
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-04 09:25:44 +0100

    add changes

--- Data/posts.js
+++ Data/posts.js
@@ -1,31 +1,31 @@
 const posts = [
     {
         titolo: 'Giornata al mare',
-        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        contenuto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam repellat deserunt natus reiciendis, nesciunt eaque saepe minima eligendi aliquid modi vitae dignissimos magni similique, quidem architecto nulla unde obcaecati!',
         immagine: './img/mare.jpg',
         tags: ['sun', 'sea', 'summer', 'holiday']
     },
     {
         titolo: 'Giornata al parco',
-        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        contenuto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam repellat deserunt natus reiciendis, nesciunt eaque saepe minima eligendi aliquid modi vitae dignissimos magni similique, quidem architecto nulla unde obcaecati!',
         immagine: './img/parco.jpg',
         tags: ['sunny', 'freedom', 'hobby', 'weekend']
     },
     {
         titolo: 'Pizza fatta in casa',
-        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        contenuto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam repellat deserunt natus reiciendis, nesciunt eaque saepe minima eligendi aliquid modi vitae dignissimos magni similique, quidem architecto nulla unde obcaecati!',
         immagine: './img/pizza.jpg',
         tags: ['pizza', 'free-time', 'cooking', 'Italia', 'MammaMia!']
     },
     {
         titolo: 'Imparando JavaScript',
-        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        contenuto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam repellat deserunt natus reiciendis, nesciunt eaque saepe minima eligendi aliquid modi vitae dignissimos magni similique, quidem architecto nulla unde obcaecati!',
         immagine: './img/javascript.jpg',
         tags: ['happy', 'PC', 'hardwork', 'super']
     },
     {
         titolo: 'Andando dallo psicologo',
-        contenuto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        contenuto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam repellat deserunt natus reiciendis, nesciunt eaque saepe minima eligendi aliquid modi vitae dignissimos magni similique, quidem architecto nulla unde obcaecati!',
         immagine: './img/psicologo.jpg',
         tags: ['JS', 'esausto', 'grazieFabio', 'happy']
     },
